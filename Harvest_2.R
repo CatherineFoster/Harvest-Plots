fa486aa89d28cf885a69da7c454ed229fb240727
Catherine Foster <fosterc3@cardiff.ac.uk>
2020-02-11 09:07:13 +0000

    Add files via upload

--- Harvest_2.R
+++ Harvest_2.R
@@ -1,5 +1,6 @@
-####Written by Catherine Foster, August 2019
+####Written by Catherine Foster, Summer 2019
 ####fosterc6@cardiff.ac.uk
+
 
 #clear env.
 rm(list=ls())
@@ -38,49 +39,75 @@ library(matrixStats)
 library(Rcapture)
 library(ggpubr)
 #######################################################################################
-##load csv file containing data for Harvest plots
-df <- read.csv(file="S:\\MyData\\Harvest_plot_data.csv",
+
+df <- read.csv(file="S:\\Research Centres\\CASCADE\\Projects\\513323 Systematic Reviews\\Family Group Conferencing\\Report\\Harvest_Plots\\Harvest_Plot_data_v5.csv",
                header=TRUE, sep=",",stringsAsFactors = TRUE)
 
-##Subset to just plot 1 outcome measure
-Care_Entry <- df[ which(df$Outcome=='1'), ]
+df <- read.csv(file="S:\\Research Centres\\CASCADE\\Projects\\513323 Systematic Reviews\\Family Group Conferencing\\Report\\Harvest_Plots\\HP_bytype.csv",
+               header=TRUE, sep=",",stringsAsFactors = TRUE)
 
-##Make it a data frame - not strictly necessary
+Care_Entry <- df[ which(df$MeetType=="TDM" & df$Outcome=="3"), ]
+#Care_Entry <- df[ which(df$Outcome=="1"), ]
 df_CE <- data.frame(Care_Entry)
 
-##Order left to right on Harvest plot
 df_CE$Effect <- factor(df_CE$Effect, levels = c("Intervention", "No Difference", "Control"))
-
-###Not strictly necessary but reminder of levels present
 df_CE$ROB <- factor(df_CE$ROB, levels = c("High", "Moderate", "Low"))
 
-###ggplot with facets for Effect
-ggplot(df_CE, aes(x=Number, y=Qual_num, fill=ROB,width=3)) +
+
+ggplot(df_CE, aes(x=Number, y=Qual_num, fill=ROB,width=3.5)) +
   geom_bar(stat="identity",position = position_dodge2(preserve="single", padding=.6,width=1)) +
   geom_hline(yintercept=df$Qual_num, linetype="solid", color = "black") +
   facet_grid(~Effect,drop=FALSE) + 
   xlab('') + ylab('') +
   scale_y_continuous(breaks=NULL) +
-  #ggtitle("Are shared decision-making family meetings effective in reducing \n out-of-home placements in families of children <18?") +
-  ggtitle("Are shared decision-making family meetings effective at \n improving family satisfaction with child welfare services?") +
-  #ggtitle("Are shared decision-making family meetings effective at \n improving family empowerment ?") +
- #ggtitle("Are shared decision-making family meetings meetings effective in reducing \n care re-entry in families of children <18?") +
-  #ggtitle("Are shared decision-making family meetings effective at increasing \n reunification in families of children <18?") +
-  #ggtitle("Do shared decision-making family meetings result in fewer referrals for child maltreatment?") +
-  scale_fill_manual(values=c("red", "yellow", "green"))+
-  theme(plot.title = element_text(hjust = 0.5,face="bold"),
+  #ggtitle("Which Service (Shared Decision-Making Meetings or Control Services) \n Had Fewer Out of Home Placements?") +
+  #ggtitle("Which Service Had Fewer Instances of Care Re-Entry?") +
+  #ggtitle("Which Service Resulted in More Children Returning Home?") +
+  #ggtitle("Which Service Resulted in Greater Family Empowerment?") +
+  #ggtitle("Which Service Resulted in Greater Family Satisfaction \n with Child Welfare Services?") +
+  #ggtitle("Which Service Resulted in Fewer Referrals for Child Maltreatment?") +
+  #ggtitle("Do Family Team Conferences Reduce Care Entry \n When Compared with Control Services?") +
+  ggtitle("Do Team Decision Making Meetings Result in More Children \n Returning Home When Compared with Control Services?") +
+  #ggtitle("Do Team Decision Making Meetings Result in Fewer Referrals \n for Child Maltreatment When Compared with Control Services?") +
+  scale_fill_manual(values=c( "#ff7057", "#5b9b54","#88d0d9"))+
+  theme(plot.title = element_text(hjust = 0.5,face="bold", size=14),
         axis.title.y = element_text(size=11, face="bold"), axis.title.x = element_text(face="bold"),
         axis.ticks = element_blank(),
         strip.text = element_text(face="bold", size=12),
         legend.text=element_text(face="bold", size=12),legend.title = element_text(face="bold", size=14), 
         axis.text.x = element_blank(), 
         axis.text.y = element_text(size=11,face="bold")) +  
-  geom_text(aes(label=StudyNumber), position=position_dodge2(padding=.6, width=1), vjust=2,hjust=0.5,fontface="bold") +
-  geom_text(aes(label=Asterisk),position=position_dodge2(padding=.6, width=2), vjust=-0.5,hjust=0.5,fontface="bold") +
+  geom_text(aes(label=StudyNumber), position=position_dodge2(padding=.6, width=1), vjust=2,hjust=0.5,fontface="bold",size=3) +
+  geom_text(aes(label=Asterisk),position=position_dodge2(padding=.6, width=2), vjust=-0.5,hjust=0.5,fontface="bold", size=3) +
   guides(fill=guide_legend(title="Risk of Bias")) 
 
 
 
 
+#ggplot(df_CE, aes(x=Number, y=Qual_num, fill=ROB,width=6)) +
+#geom_bar(stat="identity",position = position_dodge2(preserve="single", padding=.6,width=1)) +
+#geom_hline(yintercept=df$Qual_num, linetype="solid", color = "black") +
+#facet_grid(~Effect,drop=FALSE) + 
+#xlab('') + ylab('') +
+#scale_y_continuous(breaks=NULL) +
+#ggtitle("Do Family Group Decision Making Meetings Reduce Care Entry When Compared with Control Services?") +
+#ggtitle("Do FTC Meetings Result in More Children Returning Home \n When Compared with Control Services?") +
+#ggtitle("Do FUM Meetings Result in Fewer Referrals for Maltreatment \n When Compared with Control Services?") +
+#ggtitle("Are shared decision-making family meetings effective in reducing \n out-of-home placements in families of children <18?") +
+#ggtitle("Are shared decision-making family meetings effective at \n improving family empowerment ?") +
+#ggtitle("Are shared decision-making family meetings meetings effective in reducing \n care re-entry in families of children <18?") +
+#ggtitle("Are shared decision-making family meetings effective at increasing \n reunification in families of children <18?") +
+#ggtitle("Do shared decision-making family meetings result in fewer referrals for child maltreatment?") +
+#scale_fill_manual(values=c("red", "yellow", "green"))+
+#theme(plot.title = element_text(hjust = 0.5,face="bold"),
+#  axis.title.y = element_text(size=11, face="bold"), axis.title.x = element_text(face="bold"),
+#  axis.ticks = element_blank(),
+#  strip.text = element_text(face="bold", size=12),
+#  legend.text=element_text(face="bold", size=12),legend.title = element_text(face="bold", size=14), 
+#  axis.text.x = element_blank(), 
+#  axis.text.y = element_text(size=11,face="bold")) +  
+# geom_text(aes(label=StudyNumber), position=position_dodge2(padding=.6, width=1), vjust=2,hjust=0.5,fontface="bold") +
+# geom_text(aes(label=Asterisk),position=position_dodge2(padding=.6, width=2), vjust=-0.5,hjust=0.5,fontface="bold") +
+# guides(fill=guide_legend(title="Risk of Bias")) 
 
-
+#########################################################################################
